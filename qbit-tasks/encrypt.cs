42aa1b83315d6b8ff3b27b097e07a25022dc4dd9
Roman Khussein <romanfarri@gmail.com>
2023-11-21 12:23:37 +0200

    дешифрування

---
+++ qbit-tasks/encrypt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int length = int.Parse(Console.ReadLine());
+		string[] str = Console.ReadLine().Trim().Split();
+		for (int i = 0; i < length; i++)
+		{
+		
+			int a = int.Parse(str[i]);
+			Console.Write(Convert.ToChar(a));
+
+
+		}
+		
+
+	}
+}
