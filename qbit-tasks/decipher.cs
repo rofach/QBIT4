99ee021f0005643edfe7f02ddef49f6816fde798
Roman Khussein <romanfarri@gmail.com>
2023-11-21 11:33:01 +0200

    decipher

---
+++ qbit-tasks/decipher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		int length = int.Parse(Console.ReadLine());
+		string str = Console.ReadLine();
+		for (int i = 0; i < length; i++)
+		{
+			Console.Write((int)(str[i]) + " ");
+
+		}
+
+	}
+}
